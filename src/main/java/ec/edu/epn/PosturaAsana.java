102eb16c085812983832ae8bb3a07970cb144010
Sebastián <alejandro.jimenez@epn.edu.ec>
2023-12-07 22:08:09 -0500

    chore: eliminacion de los morfemas en PosturaAsana.java y añadido del glosario completo de posturas

--- src/main/java/ec/edu/epn/PosturaAsana.java
+++ src/main/java/ec/edu/epn/PosturaAsana.java
@@ -3,14 +3,12 @@ package ec.edu.epn;
 public class PosturaAsana {
     private String sanscrito;
     private String ingles;
-
     private String español;
     private String palabrasBase;
 
-    public PosturaAsana(String sanscrito, String ingles, String español, String palabrasBase) {
-        this.sanscrito=sanscrito;
-        this.palabrasBase=palabrasBase;
-        this.ingles=ingles;
+    public PosturaAsana(String sanscrito, String ingles, String español) {
+        this.sanscrito = sanscrito;
+        this.ingles = ingles;
         this.español = español;
     }
 
@@ -24,20 +22,8 @@ public class PosturaAsana {
 
     @Override
     public String toString() {
-        return "Postura:\n" +
-                "Sanscrito='" + sanscrito +"\n"
-                + "Ingles='" + ingles +"\n"
-                + "Español='" + español +"\n"
-                + "PalabrasBase: \n"+
-                presentarTraducionPalabrasBase(this.palabrasBase);
-    }
-    public String presentarTraducionPalabrasBase(String palabrasBase){
-        String resultado="";
-        String[] traducion = palabrasBase.split(";");
-        int i=0;
-        for(i=0;i<traducion.length;i++){
-            resultado+=traducion[i]+"\n";
-        }
-        return  resultado;
+        return "Traducción de la postura: " + sanscrito +
+                "\n==> Inglés: " + ingles +
+                "\n==> Español: " + español;
     }
 }
