c90d29de7d6f322aa58c2701529cc3d9a63c9bac
BryanLR <108194629+bryanLRx@users.noreply.github.com>
2023-12-08 19:30:56 -0500

    style: agregar nueva GUI

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -1,22 +1,9 @@
 package ec.edu.epn;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import ec.edu.epn.visual.PantallaInicial;
 public class Main {
     public static void main(String[] args) {
-        Diccionario diccionario = new Diccionario();
-
-        /*System.out.println(diccionario.agregarPostura(
-                "Parivrtta Trikonasana",
-                "Revolved Triangle Pose",
-                "Postura del Triángulo Invertido"));
-
-        System.out.println(diccionario.agregarMorfema(
-                "Asana",
-                "posture",
-                "postura"));*/
-
-        System.out.println(diccionario.buscarPostura("lso"));
-        System.out.println(diccionario.buscarMorfema("hasta"));
+        PantallaInicial p = new PantallaInicial();
+        p.crearFrame();
     }
 }
